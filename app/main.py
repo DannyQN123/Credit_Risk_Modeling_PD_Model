08bf91549c422b2ce81d2896c5b60f2d2c4ce88a
DannyQN123 <107457149+DannyQN123@users.noreply.github.com>
2024-10-19 17:39:50 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from prediction_helper import predict  # Ensure this is correctly linked to your
 
 # Set the page configuration and title
 st.set_page_config(page_title="Credit Risk Modeling: PD Model ", page_icon="📊")
-st.title("redit Risk Modeling: PD Model")
+st.title("Credit Risk Modeling: PD Model")
 
 # Create rows of three columns each
 row1 = st.columns(3)
@@ -15,9 +15,9 @@ row4 = st.columns(3)
 with row1[0]:
     age = st.number_input('Age', min_value=18, step=1, max_value=100, value=28)
 with row1[1]:
-    income = st.number_input('Income', min_value=0, value=1200000)
+    income = st.number_input('Income (Rupee)(0-1200000)', min_value=0, value=1200000)
 with row1[2]:
-    loan_amount = st.number_input('Loan Amount', min_value=0, value=2560000)
+    loan_amount = st.number_input('Loan Amount (Rupee)(0-2560000)', min_value=0, value=2560000)
 
 # Calculate Loan to Income Ratio and display it
 loan_to_income_ratio = loan_amount / income if income > 0 else 0
@@ -29,12 +29,12 @@ with row2[0]:
 with row2[1]:
     loan_tenure_months = st.number_input('Loan Tenure (months)', min_value=0, step=1, value=36)
 with row2[2]:
-    avg_dpd_per_delinquency = st.number_input('Avg DPD', min_value=0, value=20)
+    avg_dpd_per_delinquency = st.number_input('Average Days Past Due (Days)(0-20)', min_value=0, value=20)
 
 with row3[0]:
-    delinquency_ratio = st.number_input('Delinquency Ratio', min_value=0, max_value=100, step=1, value=30)
+    delinquency_ratio = st.number_input('Delinquency Ratio (%)(0-100)', min_value=0, max_value=100, step=1, value=30)
 with row3[1]:
-    credit_utilization_ratio = st.number_input('Credit Utilization Ratio', min_value=0, max_value=100, step=1, value=30)
+    credit_utilization_ratio = st.number_input('Credit Utilization Ratio (%)(0-100)', min_value=0, max_value=100, step=1, value=30)
 with row3[2]:
     num_open_accounts = st.number_input('Open Loan Accounts', min_value=1, max_value=4, step=1, value=2)
 
